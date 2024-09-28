555ef047ebb76b7d48d064d87cdb389d0b22de41
kevinrodbe <kevinrodbe@users.noreply.github.com>
2024-09-28 01:20:43 -0500

    fix(config): sh paths

--- unix/sh/main-init.sh
+++ unix/sh/main-init.sh
@@ -1,8 +1,7 @@
-source $HOME/.config/yo-sh/java-init.sh
-source $HOME/.config/yo-sh/alias-init.sh
-source $HOME/.config/yo-sh/tmux-init.sh
-source $HOME/.config/yo-sh/fzf-init.sh
-source $HOME/.config/yo-sh/enhancd-init.sh
-source $HOME/.config/yo-sh/sii-init.sh
-source $HOME/.config/yo-sh/ssl-init.sh
-source $HOME/.config/yo-sh/node-init.sh
+source $HOME/.config/rodbe-config/unix/sh/java-init.sh
+source $HOME/.config/rodbe-config/unix/sh/alias-init.sh
+source $HOME/.config/rodbe-config/unix/sh/tmux-init.sh
+source $HOME/.config/rodbe-config/unix/sh/fzf-init.sh
+source $HOME/.config/rodbe-config/unix/sh/enhancd-init.sh
+source $HOME/.config/rodbe-config/unix/sh/ssl-init.sh
+source $HOME/.config/rodbe-config/unix/sh/node-init.sh
